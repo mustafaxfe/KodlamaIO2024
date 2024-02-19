dd23b5cd7ac342cbd974a6edd4f7ccce8c47736a
mustafa <mustafaxfe@gmail.com>
2024-02-19 18:45:26 +0300

    added hw2

---
+++ Day2/Hw2/CSharpOOP/Constructors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Constructors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer { Id = 1, FirstName = "Engin", LastName = "Demirog", City = "Ankara" };
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            Customer customer2 = new Customer(2, "Derin", "Demirog", "Ankara");
+
+            Console.WriteLine(customer2.FirstName);
+
+        }
+    }
+    class Customer
+    {
+        //defalut constructor
+        public Customer()
+        {
+
+        }
+        public Customer(int id, string firstName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = firstName;
+            LastName = lastName;
+            City = city;
+                
+        }
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string City { get; set; }
+    }
+}
---
+++ Day2/Hw2/CSharpOOP/Interfaces/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace Interfaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // interface new'lenemez
+            //IPersonManager customerManager = new CustomerManager();
+            //customerManager.Add();
+
+            //IPersonManager employeeManager = new EmployeeManager();
+            //employeeManager.Add();
+            ProjectManager projectManager = new ProjectManager();
+            projectManager.Add(new InternManager());
+
+        }
+    }
+    interface IPersonManager
+    {
+        //unimplemented operation
+        void Add();
+        void Update();
+    }
+    //inherits - class ---------- implements - interface
+    class CustomerManager : IPersonManager
+    {
+        public void Add()
+        {
+            //müşteri ekleme kodları
+            Console.WriteLine("Müşteri Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Müşteri güncellendi");
+        }
+    }
+    class EmployeeManager : IPersonManager
+    {
+        public void Add()
+        {
+            //personel ekleme kodları
+            Console.WriteLine("Personel Eklendi");
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Personel güncellendi");
+
+        }
+
+    }
+    class InternManager : IPersonManager
+    {
+        public void Add()
+        {
+            Console.WriteLine("Stajer eklendi");
+
+        }
+
+        public void Update()
+        {
+            Console.WriteLine("Stajer güncellendi");
+        }
+    }
+
+    class ProjectManager
+    {
+        public void Add(IPersonManager personManager)
+        {
+            //CustomerManager customerManager = new CustomerManager();
+            personManager.Add();
+        }
+    }
+}
